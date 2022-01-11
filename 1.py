c26af84c048404d58acc88c864378998d210f7aa
unknown <sinelnikov@gmail.com>
2022-01-11 17:41:32 +0300

    хуй

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
-print("Main")
+print("Hello World")
+
+
+"""  git checkout <имя ветки> - сменить ветку
+git status
+git add <имя файла>
+git commit <комментарий>
+git push
+git switch <имя ветки> - - сменить ветку
+"""
