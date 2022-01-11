3eebb8301fc01a26abc4c37f82e0c9e4e044ac21
unknown <sinelnikov@gmail.com>
2022-01-11 17:43:21 +0300

    ОК

--- 1.py
+++ 1.py
@@ -4,7 +4,7 @@ print("Hello World")
 """  git checkout <имя ветки> - сменить ветку
 git status
 git add <имя файла>
-git commit <комментарий>
+git commit -m <комментарий>
 git push
 git switch <имя ветки> - - сменить ветку
 """
