21956cd4f7d180f1cfce30e3591b9e5915ade2cf
DESKTOP-AI2VHC0\user <smtulya7@gmail.com>
2022-01-21 11:13:59 +0300

    112

---
+++ 3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+i = 5
+print(i)
+i = i + 1
+print(i)
+s = '''Это многострочная строка.
+Это вторая её строчка.'''
+print("cnhjrf",s,"wwwww")
+print(4//3)
+print(-4//3)
+length = 5
+breadth = 2
+area = length * breadth
+print('Площадь равна', area)
+print('Периметр равен', 2 * (length + breadth))
