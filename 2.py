a0958faa333393040bfce08c9ea3ebd100cb6a9c
sinelnikov7 <84764934+sinelnikov7@users.noreply.github.com>
2022-05-30 19:27:10 +0300

    Update 2.py
    gfgfgfgfgf

--- 2.py
+++ 2.py
@@ -3,4 +3,4 @@ name = str(input("Как тебя ховут? "))
 if name == "Вова" or  name == "Вован" :   
     print(f"Привет  {name} !")
 else:
-    print(f"Пошел на хуй {name}!")
+    print(f"Пошел на {name}!")
